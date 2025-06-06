812c189b18aa01c0542151793b3a4b613bc6f2f5
Ornnicha.Phu <tarns2545@gmail.com>
2025-06-06 07:57:57 +0700

    Update style

--- my-first-nextjs/src/app/components/Column.tsx
+++ my-first-nextjs/src/app/components/Column.tsx
@@ -3,7 +3,6 @@
 import { ButtonData } from '../types/ButtonData'
 import ButtonItem from './ButtonItem'
 
-//TODO center the column header and shadow it
 interface ColumnProps {
   header?: string
   buttons: ButtonData[]
@@ -12,8 +11,16 @@ interface ColumnProps {
 
 export default function Column({ header, buttons, onChangeColumn }: ColumnProps) {
   return (
-    <div className="flex flex-col gap-2 border p-4 rounded w-full md:w-1/3">
-      {header && <h2 className="text-lg font-semibold mb-2">{header}</h2>}
+    <div
+      className={`flex flex-col gap-2 p-4 rounded w-full md:w-1/3 h-[600px] overflow-y-auto 
+        ${header ? 'border border-gray-300 dark:border-gray-600' : ''}`}
+      >
+      {header && (
+        <div className="bg-gray-200 dark:bg-gray-700 text-gray-900 
+        dark:text-gray-100 text-center font-semibold py-2 -m-4 mb-0 rounded-t">
+          {header}
+        </div>
+      )}
       {buttons.map(button => (
         <ButtonItem
           key={button.id}
--- my-first-nextjs/src/app/layout.tsx
+++ my-first-nextjs/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Container from "./components/Container";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <Header />
+           {/* <Container fullHeight>
+            {children}
+            </Container> */}
           {children}
-          <Footer />
+          {/* <Footer /> */}
       </body>
     </html>
   );
